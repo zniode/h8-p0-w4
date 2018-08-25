2fa51c40988e202921c0680d9d019012316e607a
zniode <zakaria.niode@gmail.com>
2018-08-25 13:33:33 +0700

    Revisi exercise 4

--- exercise-4.js
+++ exercise-4.js
@@ -1,41 +1,47 @@
 function cariModus(arr) {
-    // you can only write your code here!
-    var frequency = 0;
+
+    var index = 0;
     var greatestFrequency = 0;
-    var modus = 0;
+    var frequency2 = 0;
+    
   
     for(var i = 0; i < arr.length; i ++) {
+      var frequency = 0;
       for(var j = 0; j < arr.length; j ++) { 
         if(arr[i] === arr[j] && i !== j) {
           frequency ++;
-          // console.log([i]+ '-' + [j]);
+          frequency2 ++;
+          // console.log(arr[i]+ '-' + arr[j]);
+          // console.log(frequency)
           if(frequency > greatestFrequency) {
             greatestFrequency = frequency;
-            modus = i;
+            index = i;
             // console.log(greatestFrequency)
-            // console.log(modus)
+            // console.log(index)
           }
         }
       }
     }
     
-    if(modus === 0) {
+      
+    if(greatestFrequency === 0) {
       return -1;
     }
-    
-    var sameNumber= 0;
-    for(var k = 0; k < arr.length; k ++) {
-      sameNumber += arr[k];
-      if(sameNumber/arr.length === arr[k]) {
-        return -1;
-      }
+      
+    if(frequency2 === ((arr.length-1)*arr.length)) {
+      return -1
     }
-    return arr[modus];
+    
+    return arr[index];
   }
+    
   
-  // // TEST CASES
-  console.log(cariModus([10, 4, 5, 2, 4])); // 4
-  console.log(cariModus([5, 10, 10, 6, 5])); // 5
-  console.log(cariModus([10, 3, 1, 2, 5])); // -1
-  console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
-  console.log(cariModus([7, 7, 7, 7, 7])); // -1
+    
+    // TEST CASES
+    console.log(cariModus([5, 5, 10, 4, 5, 2, 4])); // 5
+    console.log(cariModus([5, 10, 10, 6, 5, 6, 6])); // 6
+    console.log(cariModus([10, 4, 5, 2, 4])); // 4
+    console.log(cariModus([5, 10, 10, 6, 5])); // 5
+    console.log(cariModus([10, 3, 1, 2, 5])); // -1
+    console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+    console.log(cariModus([7, 7, 7, 7, 7])); // -1
